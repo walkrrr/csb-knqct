0b2e7a1b028ab79693e8e5d6e92167bb8751d9db
Code Sandbox <hello@codesandbox.io>
2021-10-21 05:01:55 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f3226bc152deb44ee3617f62e43ed17f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2e430ef526064440919",
+    "url": "https://walkrrr.github.io/csb-knqct/static/css/main.538e82e3.chunk.css"
+  },
+  {
+    "revision": "a2e430ef526064440919",
+    "url": "https://walkrrr.github.io/csb-knqct/static/js/main.b536c01e.chunk.js"
+  },
+  {
+    "revision": "e83900630bdb4f070e4d",
+    "url": "https://walkrrr.github.io/csb-knqct/static/js/runtime~main.5468d736.js"
+  },
+  {
+    "revision": "5dfc75dccefdaf48ca84",
+    "url": "https://walkrrr.github.io/csb-knqct/static/js/2.e3341446.chunk.js"
+  },
+  {
+    "revision": "7b4d685f94695777c620faaf1e068650",
+    "url": "https://walkrrr.github.io/csb-knqct/index.html"
+  }
+];
